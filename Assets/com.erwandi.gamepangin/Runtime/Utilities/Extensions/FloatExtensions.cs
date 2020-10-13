223d531ddc70fe6a1d21d9abb29138cb4a3bfbd0
Reynold Erwandi <reynolderwandi@gmail.com>
2020-10-13 20:14:19 +0700

    Add float extensions

---
+++ Assets/com.erwandi.gamepangin/Runtime/Utilities/Extensions/FloatExtensions.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Erwandi.Gamepangin.Utilities
+{
+    /// <summary>
+    /// Extension methods for Float type
+    /// </summary>
+    public static class FloatExtensions
+    {
+        /// <summary>
+        /// Rounds down a float.
+        /// </summary>
+        /// <param name="number">Float number</param>
+        /// <param name="decimalPlaces">Decimal places</param>
+        /// <returns></returns>
+        public static float RoundDown(this float number, int decimalPlaces)
+        {
+            return Mathf.Floor(number * Mathf.Pow(10, decimalPlaces)) / Mathf.Pow(10, decimalPlaces);
+        }
+    }
+}
