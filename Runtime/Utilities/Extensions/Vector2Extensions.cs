fb8f749093d5fae228c7cc09af247514946b208f
Reynold Erwandi <reynolderwandi@gmail.com>
2020-10-13 20:42:16 +0700

    Add Vector2 and Vector3 extensions

---
+++ Runtime/Utilities/Extensions/Vector2Extensions.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+namespace Erwandi.Gamepangin.Utilities
+{
+    /// <summary>
+    /// Extension methods for Vector2 type
+    /// </summary>
+    public static class Vector2Extensions
+    {
+        /// <summary>
+        /// Rotates a Vector2 by angle in degrees.
+        /// </summary>
+        /// <param name="vector"></param>
+        /// <param name="angleInDegrees"></param>
+        /// <returns></returns>
+        public static Vector2 Rotate(this Vector2 vector, float angleInDegrees)
+        {
+            var sin = Mathf.Sin(angleInDegrees * Mathf.Deg2Rad);
+            var cos = Mathf.Cos(angleInDegrees * Mathf.Deg2Rad);
+            var tx = vector.x;
+            var ty = vector.y;
+            vector.x = (cos * tx) - (sin * ty);
+            vector.y = (sin * tx) + (cos * ty);
+            return vector;
+        }
+        
+        /// <summary>
+        /// Set X part of a Vector2.
+        /// </summary>
+        /// <param name="vector"></param>
+        /// <param name="newValue"></param>
+        /// <returns></returns>
+        public static Vector2 SetX(this Vector2 vector, float newValue)
+        {
+            vector.x = newValue;
+            return vector;
+        }
+        
+        /// <summary>
+        /// Set Y part of a Vector2.
+        /// </summary>
+        /// <param name="vector"></param>
+        /// <param name="newValue"></param>
+        /// <returns></returns>
+        public static Vector2 SetY(this Vector2 vector, float newValue)
+        {
+            vector.y = newValue;
+            return vector;
+        }
+    }
+}
---
+++ Runtime/Utilities/Extensions/Vector3Extensions.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace Erwandi.Gamepangin.Utilities
+{
+    /// <summary>
+    /// Extension methods for Vector3 type
+    /// </summary>
+    public static class Vector3Extensions
+    {
+        /// <summary>
+        /// Set X part of a Vector3.
+        /// </summary>
+        /// <param name="vector"></param>
+        /// <param name="newValue"></param>
+        /// <returns></returns>
+        public static Vector3 SetX(this Vector3 vector, float newValue)
+        {
+            vector.x = newValue;
+            return vector;
+        }
+        
+        /// <summary>
+        /// Set Y part of a Vector3.
+        /// </summary>
+        /// <param name="vector"></param>
+        /// <param name="newValue"></param>
+        /// <returns></returns>
+        public static Vector3 SetY(this Vector3 vector, float newValue)
+        {
+            vector.y = newValue;
+            return vector;
+        }
+        
+        /// <summary>
+        /// Set Z part of a Vector3.
+        /// </summary>
+        /// <param name="vector"></param>
+        /// <param name="newValue"></param>
+        /// <returns></returns>
+        public static Vector3 SetZ(this Vector3 vector, float newValue)
+        {
+            vector.z = newValue;
+            return vector;
+        }
+    }
+}
