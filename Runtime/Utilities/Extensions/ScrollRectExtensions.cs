2e3442d1d019883d19648399828a7d38516de624
Reynold Erwandi <reynolderwandi@gmail.com>
2020-10-13 20:36:49 +0700

    Add ScrollRect and RectTransform extensions

---
+++ Runtime/Utilities/Extensions/ScrollRectExtensions.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Erwandi.Gamepangin.Utilities
+{
+    /// <summary>
+    /// Extension methods for ScrollRect type
+    /// </summary>
+    public static class ScrollRectExtensions
+    {
+        /// <summary>
+        /// Scrolls a scroll rect to the top.
+        /// </summary>
+        /// <param name="scrollRect"></param>
+        public static void ScrollToTop(this ScrollRect scrollRect)
+        {
+            scrollRect.normalizedPosition = new Vector2(0, 1);
+        }
+        
+        /// <summary>
+        /// Scrolls a scroll rect to the bottom.
+        /// </summary>
+        /// <param name="scrollRect"></param>
+        public static void ScrollToBottom(this ScrollRect scrollRect)
+        {
+            scrollRect.normalizedPosition = new Vector2(0, 0);
+        }
+    }
+}
