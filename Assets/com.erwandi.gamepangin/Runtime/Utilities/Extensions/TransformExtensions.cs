bd67e7219c23e5e1657a7944bedb35932155eceb
Reynold Erwandi <reynolderwandi@gmail.com>
2020-10-13 20:38:35 +0700

    Add Transform extensions

---
+++ Assets/com.erwandi.gamepangin/Runtime/Utilities/Extensions/TransformExtensions.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Erwandi.Gamepangin.Utilities
+{
+    /// <summary>
+    /// Extension methods for Transform type
+    /// </summary>
+    public static class TransformExtensions
+    {
+        /// <summary>
+        /// Destroy all children on a transform.
+        /// </summary>
+        /// <param name="transform"></param>
+        public static void DestroyAllChildren(this Transform transform)
+        {
+            for (var t = transform.childCount - 1; t >= 0; t--)
+            {
+                if (Application.isPlaying)
+                {
+                    Object.Destroy(transform.GetChild(t).gameObject);
+                }
+                else
+                {
+                    Object.DestroyImmediate(transform.GetChild(t).gameObject);
+                }
+            }
+        }
+    }
+}
