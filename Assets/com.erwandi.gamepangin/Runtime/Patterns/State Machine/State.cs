f4548d386d1e79b4700ce5fae4c301510a28e32b
Reynold Erwandi <reynolderwandi@gmail.com>
2020-10-14 22:21:55 +0700

    Add state machine pattern

---
+++ Assets/com.erwandi.gamepangin/Runtime/Patterns/State Machine/State.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Erwandi.Gamepangin.Patterns {
+    public class State : MonoBehaviour
+    {
+        public string StateName => gameObject.name;
+
+        public UnityEvent onStateEnter;
+        public UnityEvent onStateExit;
+    }
+}
